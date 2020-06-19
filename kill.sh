aa563ff4cfc4256ffa319b2b8c05ae223e9c03c9
Ricardo <1-root@users.noreply.gitlab.sshug.cn>
2020-06-19 17:55:03 +0800

    管理脚本

---
+++ kill.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+files=$(ls ${path}/pid)
+for filename in $files
+do
+    kill -9 ${filename})
+    rm ${path}/pid/${filename}
+done
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+path=$(dirname $(readlink -f "$0"))
+if [ ! -d ${path}/pid ]; then
+    mkdir ${path}/pid
+fi
+
+files=$(ls ${path}/pid)
+for filename in $files
+do
+    kill -9 ${filename})
+    rm ${path}/pid/${filename}
+done
+
+nohup sudo python3 ${path}/main.py > /dev/null 2>&1 echo  $! > ${path}/pid/$!
